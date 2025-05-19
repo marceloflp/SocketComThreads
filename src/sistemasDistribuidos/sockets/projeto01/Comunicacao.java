c70c0436e0ba5c01b0b255c4ad2ae1043148d7fc
marceloflp <mfelipedasilva2020@gmail.com>
2025-05-19 11:52:31 -0300

    Inicializando atributos via construtor

--- src/sistemasDistribuidos/sockets/projeto01/Comunicacao.java
+++ src/sistemasDistribuidos/sockets/projeto01/Comunicacao.java
@@ -1,5 +1,30 @@
 package sistemasDistribuidos.sockets.projeto01;
 
-public class Comunicacao {
+import java.io.DataInputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
 
+public class Comunicacao implements Runnable{
+
+	private Socket conexao;
+	private DataInputStream entrada;
+	private DataOutputStream saida;
+	
+	public Comunicacao(Socket conexao, DataInputStream entrada, DataOutputStream saida) throws IOException {
+		super();
+		this.conexao = conexao;
+		this.entrada = new DataInputStream(conexao.getInputStream());
+		this.saida = new DataOutputStream(conexao.getOutputStream());
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
 }
